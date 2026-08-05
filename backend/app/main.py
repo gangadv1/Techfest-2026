e37c2bffdf42a8a746b4884588f623814fb4e5c6
Ganga Devi <gangadev001@e.ntu.edu.sg>
2026-08-05 19:21:46 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,6 +19,7 @@ app.add_middleware(
     "http://localhost:3000",
     "http://localhost:3001",
     "https://vector-theta-ebon.vercel.app",
+    "https://career-vector.vercel.app",
 ],
     allow_credentials=True,
     allow_methods=["*"],
